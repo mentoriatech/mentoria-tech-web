88f7777beb04211dbaad6636e9b51984f748f26d
Eve Montalvão <evellyn.montalvao@sumup.com>
2021-05-26 19:07:00 -0300

    feat: change deploy to serverless

--- pages/entrar/index.ts
+++ pages/entrar/index.ts
@@ -10,13 +10,11 @@ interface GetStaticProps {
 export async function getServerSideProps(
   context: unknown,
 ): Promise<GetStaticProps> {
-  const loginProviders = await providers(context)
-
   const props = {
     content: {
       title: 'mentoria.tech | entrar',
     },
-    providers: loginProviders,
+    providers: await providers(context),
   }
 
   return { props }
