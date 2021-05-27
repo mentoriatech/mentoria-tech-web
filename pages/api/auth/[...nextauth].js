c229195fd37b71d39fd643877cdd71d67d4d209a
Eve Montalvão <evellyn.montalvao@sumup.com>
2021-05-27 00:13:25 -0300

    fix GH credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,46 +5,13 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GH_APPLICATION_CLIENT_ID,
+      clientSecret: process.env.GH_APPLICATION_CLIENT_SECRET,
       scope: 'user,repo',
     }),
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    Providers.Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        const user = () => {
-          const user = credentials.username === process.env.CREDENTIALS_USERNAME
-          const password =
-            credentials.username === process.env.CREDENTIALS_PASSWORD
-
-          return (
-            user &&
-            password && { id: 1, name: 'Jane Doe', email: 'jane@doe.com' }
-          )
-        }
-        if (user) {
-          console.log(
-            '🚀 ~ file: [...nextauth].js ~ line 38 ~ authorize ~ user',
-            user,
-          )
-          // Any user object returned here will be saved in the JSON Web Token
-          return user
-        } else {
-          return null
-        }
-      },
     }),
   ],
   pages: {
